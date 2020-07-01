6d01224895f66e54b0bb0ecc12e976e4a1437eaf
cucumberbolts <53242027+cucumberbolts@users.noreply.github.com>
2020-06-30 20:10:18 -0700

    Improved readability of code.

--- xkcd_scraper.py
+++ xkcd_scraper.py
@@ -1,3 +1,7 @@
+"""
+A program for automatically downloading xkcd comics
+"""
+
 import time
 import threading
 import os
@@ -8,7 +12,8 @@ sys.path.append("./dependencies")
 from bs4 import BeautifulSoup
 
 
-def get_xkcd_image(number: int):
+def get_xkcd_source(number: int) -> tuple:
+    """ Gets the xkcd url for the comic number """
     # Reads the xkcd comic page and gets the source code
     html = requests.get("https://xkcd.com/" + str(number)).content
 
@@ -20,27 +25,29 @@ def get_xkcd_image(number: int):
     img_source = "https:" + attributes["src"]
 
     # Gets the name of the comic
-    title = "_".join(soup.find(id="ctitle").getText().split())
-
-    if "?" in title:
-        title = "".join(title.split("?"))
-    if "/" in title:
-        title = "_".join(title.split("/"))
+    title = img_source[img_source.rfind("/") + 1:]
 
     return title, img_source
 
 
-def save_xkcd_comic(number: int):
-    title, img_source = get_xkcd_image(number)
+def save_xkcd_comic(number: int) -> None:
+    """ Saves the comic as an image """
+    title, img_source = get_xkcd_source(number)
 
     # Writes image to file
     img_data = requests.get(img_source).content
-    with open("./xkcd_comics/" + title + ".png", "wb") as handler:
+    with open("./xkcd_comics/" + title, "wb") as handler:
         handler.write(img_data)
 
 
-def main(start=1, stop=10, comics=[]):
-    if (comics == []):
+def main(start: int = 1, stop: int = 10, comics: None = None) -> None:
+    """ Main function """
+    comics = []
+
+    if not os.path.isdir("xkcd_comics"):
+        os.mkdir("xkcd_comics")
+
+    if comics == []:
         comics = list(range(start, stop))
     if 404 in comics:  # 404 is not a comic
         comics.remove(404)
@@ -56,11 +63,9 @@ def main(start=1, stop=10, comics=[]):
 
 
 if __name__ == "__main__":
-    if (os.path.isdir("xkcd_comics") == False):
-        os.mkdir("xkcd_comics")
-
     before = time.time()
     main(1, 100)
     after = time.time()
     seconds = (after - before) % 60
-    print("time took: ", int(((after - before) - seconds) / 60), " minutes and ", seconds, " seconds", sep="")
+    minutes = int(((after - before) - seconds) / 60)
+    print(f"Time took: {minutes} minutes and {seconds} seconds", sep="")
