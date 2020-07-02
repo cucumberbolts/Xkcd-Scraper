40de00f7a9df914ecc21a623af22866276f09d9b
cucumberbolts <53242027+cucumberbolts@users.noreply.github.com>
2020-07-01 18:35:54 -0700

    Added command-line arguments

--- README.md
+++ README.md
@@ -1,15 +1,44 @@
 # Xkcd Scraper
 Xkcd Scraper is a small program for automatically downloading xkcd comics.
 
+---
+
 ## Installation
-1) You should have python installed on your computer.
+1) Make sure you have python installed on your computer.
 2) Download the xkcd_scraper project
 ```
 git clone https://github.com/cucumberbolts/xkcd_scraper.git
 ```
+---
 
 ## Usage
-1) Run xkcd_scraper.py
+Your can run `xkcd_scraper.py` by double clicking on it or running it in the command-line
+
 ```
 python xkcd_scraper.py
+```
+By default, this will download comics 1 to 100.
+
+### Command-line use
+
+If you want to download comics in a specific range you can do so with the `-r` or `--range` option:
+
+```
+python xkcd_scraper.py --range 100 1000
+```
+
+Or you can list out all the comics with `-l` or `--list`:
+
+```
+python xkcd_scraper.py --list 1 2 3 4 5
+```
+
+If you want the latest comic, use the `--latest` option.
+
+> Note: if you specify a range and a list, the program will use the list.
+
+The default directory for all the comics is xkcd_scraper, but you can change that with the `-o` or `--output` option:
+
+```
+python xkcd_scraper.py --output ./other_dir
 ```
--- xkcd_scraper.py
+++ xkcd_scraper.py
@@ -1,55 +1,95 @@
 """
-A program for automatically downloading xkcd comics
+A program for automatically downloading xkcd comics.
 """
 
 import time
 import threading
 import os
-import requests
 import json
+import argparse
+import requests
 
 
-def get_xkcd_source(number: int) -> tuple:
+def get_xkcd_info(number: str) -> tuple:
     """ Gets the xkcd url for the comic number """
-    # Reads the xkcd comic page and gets the source code
-    source = requests.get(F"https://xkcd.com/{str(number)}/info.0.json").content
+    source = ""
+    if number == "0":
+        source = requests.get("https://xkcd.com/info.0.json").content
+    else:
+        source = requests.get(f"https://xkcd.com/{number}/info.0.json").content
 
     data = json.loads(source)
 
     # Get's the image address of the comic
     img_source = data["img"]
 
-    # Gets the name of the comic
-    title = img_source[img_source.rfind("/") + 1:]
+    # Gets the file name of the comic
+    file_name = img_source[img_source.rfind("/") + 1:]
 
-    return title, img_source
+    return file_name, data
 
 
-def save_xkcd_comic(number: int) -> None:
+def save_xkcd_comic(number: str, output_dir: str) -> None:
     """ Saves the comic as an image """
-    title, img_source = get_xkcd_source(number)
+    file_name, data = get_xkcd_info(number)
 
     # Writes image to file
-    img_data = requests.get(img_source).content
-    with open("./xkcd_comics/" + title, "wb") as handler:
+    img_data = requests.get(data["img"]).content
+    with open(f"{output_dir}/{file_name}", "wb") as handler:
         handler.write(img_data)
 
 
-def main(start: int = 1, stop: int = 10, comics: None = None) -> None:
-    """ Main function """
-    comics = []
+def get_latest_comic() -> str:
+    """ Returns the number of the latest comic as a string"""
+    _, data = get_xkcd_info("0")
+    return str(data["num"])
 
-    if not os.path.isdir("xkcd_comics"):
-        os.mkdir("xkcd_comics")
+
+def parse_arguments() -> tuple:
+    """ Parses the command-line arguments """
+    # Default arguments
+    start, stop = 1, 100
+    comics = []
+    output_dir = "./xkcd_comics"
+
+    # New parser object
+    parser = argparse.ArgumentParser(description="A program for automatically downloading xkcd comics")
+
+    parser.add_argument("-o", "--output", help="the path to save comics", default=output_dir)
+    parser.add_argument("-r", "--range", help="range of comics", type=int, nargs=2, default=(start, stop))
+    parser.add_argument("-l", "--list", help="list of comics", type=int, nargs="+", default=comics)
+    parser.add_argument("--latest", help="latest comic", action="store_true")
+
+    args = parser.parse_args()
+
+    output_dir = args.output
+    start, stop = args.range
+    comics = args.list
+
+    if args.latest:
+        comics.append(get_latest_comic())
+
+    return start, stop, comics, output_dir
+
+
+def main(start: str, stop: str, comics: list, output_dir: str) -> None:
+    """ Main function """
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
 
     if comics == []:
-        comics = list(range(start, stop))
+        comics = list(range(int(start), int(stop)))
+
     if 404 in comics:  # 404 is not a comic
         comics.remove(404)
 
+    for comic in comics:
+        if int(comic) < 1 or int(comic) > int(get_latest_comic()):
+            raise Exception(f"Number {comic} is not a comic!")
+
     threads = []
     for comic in comics:
-        thread = threading.Thread(target=save_xkcd_comic, args=(comic,))
+        thread = threading.Thread(target=save_xkcd_comic, args=(comic, output_dir,))
         thread.start()
         threads.append(thread)
 
@@ -59,7 +99,7 @@ def main(start: int = 1, stop: int = 10, comics: None = None) -> None:
 
 if __name__ == "__main__":
     before = time.time()
-    main(1, 100)
+    main(*parse_arguments())
     after = time.time()
     seconds = (after - before) % 60
     minutes = int(((after - before) - seconds) / 60)
